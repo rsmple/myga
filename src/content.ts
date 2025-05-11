5554247a4efa1873c6b53b888dbdc92bf2472523
Rus P <texturered@gmail.com>
2025-05-11 15:23:50 +0200

    ext: add publish action

--- src/content.ts
+++ src/content.ts
@@ -2,7 +2,7 @@ chrome.storage.local.get(['gridValue'], (data) => {
   if (!data.gridValue) return
 
   const applyValue = () => {
-    document.querySelectorAll('ytd-rich-item-renderer[rendered-from-rich-grid]').forEach(el => {
+    document.querySelectorAll('ytd-rich-grid-renderer').forEach(el => {
       if (!(el instanceof HTMLElement)) return
 
       el.style.setProperty('--ytd-rich-grid-items-per-row', data.gridValue.toString(), 'important')
